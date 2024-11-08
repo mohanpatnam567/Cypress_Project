07e36d7df9f90f4c974e24e2091e1ce9fbb86be5
Mohan Patnam <mohan.patnam@Mohans-MacBook-Air.local>
2024-11-08 18:30:14 +0530

    Commiting basics of cypress

---
+++ cypress/e2e/1_Basics_Practice/Selectors.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Selectors practice',()=>{
+
+    it('Css Selectors',()=>{
+
+        cy.visit("https://www.toolsqa.com/cypress/what-is-cypress/")
+
+        
+
+
+
+
+    })
+})
